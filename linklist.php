437b8139aacbcd0b5d5e19d9a26943bd72610729
James Hamann <hamann.james87@gmail.com>
2021-03-11 14:26:47 -0800

    initial commit

---
+++ linklist.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: LinkList
+  Plugin URI: https://github.com/hamannjames/linklist
+  Description: A replacement for Linktree for wordpress sites
+  Version: 0.1
+  Requires at lease: 5.1
+  Requires PHP: 5.6
+  Author: James Hamann
+  Author URI: https://github.com/hamannjames
+  License: https://www.gnu.org/licenses/gpl-3.0.html
+*/
